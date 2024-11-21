2568aceb41d373ec9bc53d7bdd1d44a5d683229f
Rob Flynn <rob@thingerly.com>
2024-11-21 20:08:42 +0000

    Add missing accessibilities arugment to period.

--- lib/bitmovin/dash/period.rb
+++ lib/bitmovin/dash/period.rb
@@ -4,10 +4,18 @@ class Bitmovin::Dash::Period < Bitmovin::Object
   param :manifest_id, exclude: true
 
   def build_video_adaptation_set(roles: [])
-    Bitmovin::Dash::VideoAdaptationSet.new(manifest_id: self.manifest_id, period_id: self.id, roles: roles)
+    Bitmovin::Dash::VideoAdaptationSet.new(
+      manifest_id: self.manifest_id,
+      period_id: self.id, roles: roles
+    )
   end
 
-  def build_audio_adaptation_set(roles: [])
-    Bitmovin::Dash::AudioAdaptationSet.new(manifest_id: self.manifest_id, period_id: self.id, roles: roles)
+  def build_audio_adaptation_set(roles: [], accessibilities: [])
+    Bitmovin::Dash::AudioAdaptationSet.new(
+      manifest_id: self.manifest_id,
+      period_id: self.id,
+      roles: roles,
+      accessibilities: accessibilities
+    )
   end
 end
